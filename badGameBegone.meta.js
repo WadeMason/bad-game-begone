63232cd90071a0a55166634cc6fed1542b75c15e
Wade Mason <WadeMason@users.noreply.github.com>
2018-02-20 03:16:13 -0500

    Create badGameBegone.meta.js

---
+++ badGameBegone.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Bad Game Begone
+// @namespace    https://github.com/WadeMason/bad-game-begone
+// @version      1.0
+// @description  Automatically marks negatively reviewed games as "Not Interested" in Steam Queues.
+// @author       Wade Mason
+// @include      http://store.steampowered.com/app/*
+// @include      https://store.steampowered.com/app/*
+// @include      http://store.steampowered.com/explore*
+// @include      https://store.steampowered.com/explore*
+// @include      http://store.steampowered.com/agecheck/app/*
+// @include      https://store.steampowered.com/agecheck/app/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/WadeMason/bad-game-begone/master/badGameBegone.user.js
+// @updateURL    https://raw.githubusercontent.com/WadeMason/bad-game-begone/master/badGameBegone.meta.js
+// ==/UserScript==

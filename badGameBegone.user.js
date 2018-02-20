b6f296cab14d97148df62b3290d6a856054a160b
Wade Mason <WadeMason@users.noreply.github.com>
2018-02-20 03:16:39 -0500

    Update and rename badGameBegone.js to badGameBegone.user.js

--- badGameBegone.user.js
+++ badGameBegone.user.js
@@ -8,8 +8,11 @@
 // @include      https://store.steampowered.com/app/*
 // @include      http://store.steampowered.com/explore*
 // @include      https://store.steampowered.com/explore*
+// @include      http://store.steampowered.com/agecheck/app/*
+// @include      https://store.steampowered.com/agecheck/app/*
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/WadeMason/bad-game-begone/master/badGameBegone.js
+// @downloadURL  https://raw.githubusercontent.com/WadeMason/bad-game-begone/master/badGameBegone.user.js
+// @updateURL    https://raw.githubusercontent.com/WadeMason/bad-game-begone/master/badGameBegone.meta.js
 // ==/UserScript==
 
 (function()

83e0b13489964a646969ce7da49e2b37bbdf295e
Wade Mason <WadeMason@users.noreply.github.com>
2018-02-21 16:22:13 -0500

    Update badGameBegone.user.js
    Added checks for age-gated games.

--- badGameBegone.user.js
+++ badGameBegone.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bad Game Begone
 // @namespace    https://github.com/WadeMason/bad-game-begone
-// @version      1.0
+// @version      1.1
 // @description  Automatically marks negatively reviewed games as "Not Interested" in Steam Queues.
 // @author       Wade Mason
 // @include      http://store.steampowered.com/app/*
@@ -20,11 +20,11 @@
     'use strict';
 
     // Game reviews
-    const reviewRecent = (function()
+    const reviewRecent = ( function()
     {
         if(document.getElementsByClassName('game_review_summary')[0]) return document.getElementsByClassName('game_review_summary')[0].innerHTML;
     })();
-    const reviewOverall = (function()
+    const reviewOverall = ( function()
     {
         if(document.getElementsByClassName('game_review_summary')[1]) return document.getElementsByClassName('game_review_summary')[1].innerHTML;
     })();
@@ -33,7 +33,7 @@
     const notInterested = document.getElementsByClassName('queue_btn_inactive')[1];
     const continueButton = (function()
     {
-        if(document.getElementsByClassName('btn_next_in_queue')[0])
+        if( document.getElementsByClassName('btn_next_in_queue')[0] )
         {
             return document.getElementsByClassName('btn_next_in_queue')[0];
         }
@@ -43,20 +43,30 @@
             return document.getElementsByClassName('btn_next_in_queue_trigger')[0];
         }
 
-        if( document.getElementById('refresh_queue_btn') && document.getElementById('refresh_queue_btn').click)
+        if( document.getElementById('refresh_queue_btn') && document.getElementById('refresh_queue_btn').click )
         {
             return document.getElementById('refresh_queue_btn');
         }
 
         // Age Gate
+        if(document.getElementById('app_agegate'))
+        {
+            return document.getElementsByClassName('btn_medium')[0];
+        }
 
-        console.log('Not in queue');
+        if(document.getElementById('ageYear'))
+        {
+            document.getElementById('ageYear').value = 1996;
+            return document.getElementsByClassName('btn_small')[0];
+        }
+
+        console.log('Script is likely outdated');
         return null;
     })();
 
     function handlePage()
     {
-        if(badReview())
+        if( badReview() )
         {
             notInterested.click();
             continueButton.click();
@@ -69,14 +79,8 @@
 
     function badReview()
     {
-        // Check recent reviews
-        if(reviewRecent == 'Mostly Negative' || reviewRecent == 'Negative' || reviewRecent == 'Very Negative' || reviewRecent == 'Overwhelmingly Negative')
-        {
-            return true;
-        }
-
-        // Check overall reviews
-        if(reviewOverall == 'Mostly Negative' || reviewOverall == 'Negative' || reviewOverall == 'Very Negative' || reviewOverall == 'Overwhelmingly Negative')
+        // Check reviews
+        if((reviewRecent == 'Mostly Negative' && reviewOverall == 'Mostly Negative') || (reviewRecent == 'Negative' && reviewOverall == 'Negative') || (reviewRecent == 'Very Negative' && reviewOverall == 'Very Negative') || (reviewRecent == 'Overwhelmingly Negative' && reviewOverall == 'Overwhelmingly Negative'))
         {
             return true;
         }
